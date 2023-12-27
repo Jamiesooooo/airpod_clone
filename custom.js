3fcef7d679d4d2e8f8a8edbd20a18e8f17dc117d
apple <apple@appleui-iMac-2.skbroadband>
2023-12-27 11:27:41 +0900

    231227 version 0.0.1

--- custom.js
+++ custom.js
@@ -5,4 +5,19 @@ const img = document.querySelector("img");
 const a = 10;
 const b = a.toString().padStart(4, "0"); //지정된 숫자에 대해 첫번째 파라미터 길이로 두번째 parameter 문자로 채워줌(4개의 숫자중 const a 로 지정된 숫자 외에 나머지 남는 두개 숫자는 00이렇게 채워져서 0010 이런식으로 됨. 만약 (4, "a")로 하면 aa10 이렇게 된다 이렇게 해서 반복문 let for 로 지정해놓으면 프로그래밍이 되는.
 
-console.log(b);
+// console.log(b);
+
+// hero 섹션 높이 지정: 헤더 높이를 제외한 높이
+const headerHeight = document.querySelector("header").offsetHeight;
+const heroSection = document.querySelector(".section-01");
+
+heroSection.style.marginTop = headerHeight + 50 + "px";
+
+const imgObj = document.querySelector("#hero-lightpass");
+const mainText = document.querySelector(".main-elem");
+
+let tl0 = gsap.timeline();
+tl0
+  .add("start0") // 시작 지점 지정
+  .fromTo(imgObj, { opacity: 0 }, { opacity: 1, duration: 2 }, "start0")
+  .fromTo(mainText, { opacity: 0 }, { opacity: 1, duration: 2 }, "start0");
